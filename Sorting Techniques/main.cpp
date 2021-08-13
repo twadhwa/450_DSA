1b53428315f710a89b371d28c63e358f404c5ec9
Tushar Wadhwa <66528495+twadhwa@users.noreply.github.com>
2021-08-13 22:02:25 +0530

    Update main.cpp

--- Sorting Techniques/main.cpp
+++ Sorting Techniques/main.cpp
@@ -80,7 +80,54 @@ int selection_sort(int Arr[] , int n ){
     return 0 ;
 }
 
+// Merge sort
+// Merging function
+void merge(int A[],int l,int mid,int h)
+{
+    int i=l,j=mid+1,k=l;
+    int B[100];
+    while(i<=mid && j<=h)
+    {
+        if(A[i]<A[j])
+            B[k++]=A[i++];
+        else
+            B[k++]=A[j++];
+    }
+    for(;i<=mid;i++)
+        B[k++]=A[i];
+    for(;j<=h;j++)
+        B[k++]=A[j];
+    for(i=l;i<=h;i++)
+        A[i]=B[i];
+}
 
+// Merge sort main function
+
+void IMergeSort(int A[],int n)
+{
+    int p,l,h,mid,i;
+    for(p=2;p<=n;p=p*2)
+    {
+        for(i=0;i+p-1<n;i=i+p)
+        {
+            l=i;
+            h=i+p-1;
+            mid=(l+h)/2;
+            merge(A,l,mid,h);
+        }
+        if(n-i>p/2)
+        {
+            l = i;
+            h = i+p-1;
+            mid = (l+h)/2;
+            merge(A, l, mid, n-1);
+        }
+    }
+    if(p/2<n)
+    {
+        merge(A,0,p/2-1,n-1);
+    }
+}
 // Count Sort
 // The best time complexity of the sort is O(n)
 // The worst time complexity of the sort is O(n)
@@ -154,10 +201,35 @@ int bin_sort (int Arr[] , int n ){
 }
 
 
+void merge(int A[],int l,int mid,int h, int n )
+{
+    int i=l,j=mid+1,k=l;
+    int B[n];
+    while(i<=mid && j<=h)
+    {
+        if(A[i]<A[j])
+            B[k++]=A[i++];
+        else
+            B[k++]=A[j++];
+    }
+    for(;i<=mid;i++)
+        B[k++]=A[i];
+    for(;j<=h;j++)
+        B[k++]=A[j];
+    for(i=l;i<=h;i++)
+        A[i]=B[i];
+
+    for (int i = 0 ; i< n ; i++){
+        cout<<B[i]<<endl  ;
+    }
+}
+
+
 
 int main() {
-    int Arr[6] = {7,3,9,9,2,1};
-    int n =6 ;
+    int Arr[8] = {1,5,3,11,2,6,6,10};
+    int n =8    ;
+
     cout<<"Array before sorting " ;
 
     for (int i = 0 ; i < n ; i++){
@@ -165,7 +237,8 @@ int main() {
     }
 
     cout<<endl;
-    count_sort(Arr,n);
+    IMergeSort(Arr,8);
+
     cout<<"Array after sorting ";
     for (int i = 0 ; i < n ; i++){
         cout<<Arr[i]<<" ";
@@ -174,6 +247,13 @@ int main() {
 
 
 
+
+
+
+
+
+
+
+
     return 0;
 }
-
